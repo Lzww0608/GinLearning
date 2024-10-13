880a92a2f4e9946ae589dfa33575879a78b36dad
lzw <1094693070@qq.com>
2024-10-13 10:15:36 +0800

    Add URI content

--- Data parsing and binding/URI.go
+++ Data parsing and binding/URI.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// 定义接收数据的结构体
+type Login struct {
+	// binding:"required"修饰的字段，若接收为空值，则报错，是必须字段
+	User string `form:"username" json:"user" uri:"user" xml:"user" binding:"r
+equired"`
+	Password string `form:"password" json:"password" uri:"password" xml:"passwor
+d" binding:"required"`
+}
+
+func main() {
+	// 1.创建路由
+	// 默认使用了两个中间件Logger(), Recovery()
+	r := gin.Default()
+	// JSON绑定
+	r.GET("/:user/:password", func(c *gin.Context) {
+		// 声明接收的变量
+		var login Login
+		// Bind()默认解析并绑定form格式
+		// 根据请求头中content-type自动推断
+		if err := c.ShouldBindUri(&login); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if login.User != "root" || login.Password != "admin" {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "304"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "200"})
+	})
+	r.Run(":8080")
+}
+
+// curl.exe http://127.0.01:8080/root/admin
